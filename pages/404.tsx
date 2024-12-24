085749a431b3deefa996cbcabc08d1af9ead0626
Josh <jsbusiness305@gmail.com>
2024-12-23 19:54:27 -0500

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+// pages/404.js
+const Custom404 = () => {
+    return (
+      <div style={{ textAlign: 'center', padding: '2rem' }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you're looking for doesn't exist.</p>
+      </div>
+    );
+  }
+
+  export default Custom404
